372272b77766061255c021f935ee0256212656ad
Andrey Malinin <isaac220515@gmail.com>
2019-11-13 23:35:11 +0300

    Initial commit from Create React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <text>Hello World</text>
+      </div>
+    );
+  }
+}
+
+export default App;
