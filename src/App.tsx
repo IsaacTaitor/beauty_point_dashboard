08deddfc5b08a16c3b032d0b65fbc38c3b348f55
Andrey <isaac220515@gmail.com>
2019-11-14 17:58:11 +0300

    Добавлен redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
 import TimeLine from './components/TimeLine'
 import { observe } from './components/DB'
 
-class App extends Component<any, any> {
-	state = {
-		cardPosition: [0, 0]
+const initialState = {
+	cardPosition: [],
+};
+
+function Position(state = initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
 	}
+}
+
+
+
+const store = createStore(combineReducers({
+	Position
+}), applyMiddleware(logger, thunk));
+
+class App extends Component<any, any> {
 	render() {
 		return (
-			<div style={{ width: document.documentElement.clientWidth - 15, height: document.documentElement.clientHeight - 20 }}>
-				{<TimeLine cardPosition={this.state.cardPosition} />}
-			</div>
+			<Provider store={store}>
+
+				<div style={{ width: document.documentElement.clientWidth - 15, height: document.documentElement.clientHeight - 20 }}>
+					{<TimeLine cardPosition={[0]} />}
+				</div>
+			</Provider>
 		)
 	}
 
